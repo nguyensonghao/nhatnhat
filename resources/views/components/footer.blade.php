d17f3fb224313c62466599820b69d83d34347ce7
nguyensonghao94 <20121630@student.hut.edu.vn>
2020-10-12 23:36:54 +0700

    fix bug css

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,48 +1,31 @@
 <footer id="footer">
-    <div class="ft-infos">
-        <div class="all">
-            <div class="logo">
-                <div class="img">
-                    <a href="">
-                        <img src="/public/images/footer-logo.png" alt=""></a>
-                </div>
-            </div>
-            <div class="navct-wrap">
-                <div class="navct__child">
-                    <div class="navct-tt">
-                        <h4 class="hd">Công ty</h4>
+    <div class="footer-container">
+        <div class="storages">
+            <div class="list-storage">
+                <div class="storage-item">
+                    <div class="company-address">
+{{--                        <img src="https://vanchuyenphongvu.vn/wp-content/uploads/2019/08/logo-vanchuyenphongvu.png"/>--}}
+                        <p>Trụ sở: 31 Quan Hoa, Cầu Giấy, Hà Nội</p>
+                        <p>Giờ làm việc: 24/24</p>
+                        <p>090.345.0696 (Việt Nam)</p>
+                        <p>15603006855 (Trung Quốc)</p>
+                        <p>vanchuyenphongvu@gmail.com</p>
                     </div>
-                    <ul class="list-navct">
-                        <li class="navct__item"><a href="">Về chúng tôi</a></li>
-                        <li class="navct__item"><a href="">Điều khoản</a></li>
-                    </ul>
                 </div>
-                <div class="navct__child">
-                    <div class="navct-tt">
-                        <h4 class="hd">Ngành hàng</h4>
-                    </div>
-                    <ul class="list-navct">
-                        <li class="navct__item"><a href="">Thời trang nữ</a></li>
-                        <li class="navct__item"><a href="">Thời trang nam</a></li>
-                        <li class="navct__item"><a href="">Đồ dùng trẻ em</a></li>
-                        <li class="navct__item"><a href="">Đồ phượt</a></li>
-                        <li class="navct__item"><a href="">Nội thất</a></li>
-                    </ul>
+                <div class="storage-item">
+                    <p class="storage-name">Kho Hà Nội</p>
+                    <p>31 Quan Hoa, Cầu Giấy, HN</p>
+                    <p>Điện thoại: 090.345.0696</p>
                 </div>
-                <div class="navct__child">
-                    <div class="navct-tt">
-                        <h4 class="hd">Hỗ trợ</h4>
-                    </div>
-                    <ul class="list-navct">
-                        <li class="navct__item"><a href="">Hướng dẫn</a></li>
-                        <li class="navct__item"><a href="">Nạp tiền</a></li>
-                        <li class="navct__item"><a href="">Câu hỏi thường gặp</a></li>
-                    </ul>
+                <div class="storage-item">
+                    <p class="storage-name">Kho TP. HCM</p>
+                    <p>720A Điện Biên Phủ, P.22, Q. Bình Thạnh, TP. HCM</p>
+                    <p>Điện thoại: 090.345.0696</p>
                 </div>
-            </div>
-            <div class="facebook-frame">
-                <div class="img">
-                    <div class="fb-page fb_iframe_widget" data-href="https://www.facebook.com/khacdo78" data-small-header="false" data-adapt-container-width="false" data-hide-cover="false" data-show-facepile="true" fb-xfbml-state="rendered" fb-iframe-plugin-query="adapt_container_width=false&amp;app_id=&amp;container_width=269&amp;hide_cover=false&amp;href=https%3A%2F%2Fwww.facebook.com%2Fkhacdo78&amp;locale=vi_VN&amp;sdk=joey&amp;show_facepile=true&amp;small_header=false"><span style="vertical-align: bottom; width: 0px; height: 0px;"><iframe name="f25c878b598ffb4" width="1000px" height="1000px" data-testid="fb:page Facebook Social Plugin" title="fb:page Facebook Social Plugin" frameborder="0" allowtransparency="true" allowfullscreen="true" scrolling="no" allow="encrypted-media" src="https://www.facebook.com/v2.9/plugins/page.php?adapt_container_width=false&amp;app_id=&amp;channel=https%3A%2F%2Fstaticxx.facebook.com%2Fx%2Fconnect%2Fxd_arbiter%2F%3Fversion%3D46%23cb%3Df3837bab7d06d8c%26domain%3Dnhatnhatlogistics.com%26origin%3Dhttps%253A%252F%252Fnhatnhatlogistics.com%252Ff948aeb32f035%26relation%3Dparent.parent&amp;container_width=269&amp;hide_cover=false&amp;href=https%3A%2F%2Fwww.facebook.com%2Fkhacdo78&amp;locale=vi_VN&amp;sdk=joey&amp;show_facepile=true&amp;small_header=false" style="border: none; visibility: visible; width: 0px; height: 0px;" class=""></iframe></span></div>
+                <div class="storage-item">
+                    <p class="storage-name">Kho Trung Quốc</p>
+                    <p>广东省广州市越秀区越秀南路5号双启打包物流105B档口</p>
+                    <p>Điện thoại: 15603006855</p>
                 </div>
             </div>
         </div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,19 +19,7 @@
             </div>
             <div class="dark-overlay"></div>
             <div class="banner-content">
-                <div class="all">
-                    <div class="bn__header">
-                        <h1 class="hd" style="font-size: 35px; color: #fff">
-                            MỘT CHỮ TÍN - VẠN NIỀM TIN
-                        </h1>
-                    </div>
-                    <span class="line-br"></span>
-                    <div class="addon-button">
-                        <p class="button-title" style="color: #fff">Cài đặt công cụ</p>
-                        <a href="https://chrome.google.com/webstore/detail/c%C3%B4ng-c%E1%BB%A5-%C4%91%E1%BA%B7t-h%C3%A0ng-c%E1%BB%A7a-nh%E1%BA%A5t/mibogeiokpajieffeflbgkjoeokdhcdn" target="_blank" class="mn-btn gg-btn"><i class="fab fa-chrome"></i>Chrome</a>
-                        <a href="https://chrome.google.com/webstore/detail/c%C3%B4ng-c%E1%BB%A5-%C4%91%E1%BA%B7t-h%C3%A0ng-c%E1%BB%A7a-nh%E1%BA%A5t/mibogeiokpajieffeflbgkjoeokdhcdn" target="_blank" class="mn-btn cc-btn"><i class="fab fa-chrome"></i>Cốc cốc</a>
-                    </div>
-                </div>
+
             </div>
         </div>
         <div class="present">
@@ -73,163 +61,51 @@
                 </ul>
             </div>
         </div>
-        <div class="step-order">
-            <div class="all">
-                <div class="sec100-wrap">
-                    <div class="sec40-left">
-                        <div class="sec-title">
-                            <h2 class="hd">Quy trình đặt hàng</h2>
-                            <p>Businesses often become known today through effective marketing. The marketing...</p>
-                        </div>
-                        <ul class="list-navct">
-                            <li class="navct-item active">
-                                <a href="#order1" step-nav="#order1" class="navct-link">Đăng ký tài khoản</a>
-                            </li>
-                            <li class="navct-item">
-                                <a href="#order2" step-nav="#order2" class="navct-link">Cài đặt công cụ mua hàng</a>
-                            </li>
-                            <li class="navct-item">
-                                <a href="#order3" step-nav="#order3" class="navct-link">Chọn hàng và thêm vào giỏ</a>
-                            </li>
-                            <li class="navct-item">
-                                <a href="#order4" step-nav="#order4" class="navct-link">Gửi đơn hàng</a>
-                            </li>
-                            <li class="navct-item">
-                                <a href="#order5" step-nav="#order5" class="navct-link">Đặt cọc tiền hàng</a>
-                            </li>
-                            <li class="navct-item">
-                                <a href="#order6" step-nav="#order6" class="navct-link">Gửi hàng và thanh toán</a>
-                            </li>
+        <div class="services">
+            <p class="title">DỊCH VỤ</p>
+            <div class="list-service">
+                <div class="service-item">
+                    <div class="service-item-heaader">
+                        <img src="https://vanchuyenphongvu.vn/wp-content/uploads/2019/08/icon-service-2.png" alt=""/>
+                        <p class="capture">VẬN CHUYỂN HÀNG TRUNG – VIỆT</p>
+                    </div>
+                    <div class="service-item-content">
+                        <p class="sub-capture">Tối ưu quy trình vận chuyển Trung – Việt với hệ thống kho bãi tiện nghi, đưa phí vận chuyển về mức thấp trên thị trường.</p>
+                        <ul>
+                            <li>Hàng về sau 2-3 ngày</li>
+                            <li>Đền 100% nếu mất/vỡ do vận chuyển</li>
+                            <li>Hệ thống kho bãi quy mô</li>
                         </ul>
                     </div>
-                    <span class="line-devide">
-                            <span class="line-top"></span>
-                            <span class="line-bot"></span>
-                        </span>
-                    <div class="sec60-right">
-                        <div class="nav-content-wrap">
-
-                            <div class="navct-desc" id="order1">
-                                <div class="icon">
-                                    <img src="/public/images/step-icon1.png" alt="">
-                                </div>
-                                <h3 class="hd">Đăng ký tài khoản</h3>
-                                <p>
-                                    Nhập các thông tin cá nhân bắt buộc vào ô, lưu ý nhập chính xác các thông tin để đảm bảo cho việc quản lí đơn hàng và nhận hàng của bạn.
-                                </p>
-                                <a href="/dang-ky" class="mn-btn btn-bg">Đăng ký</a>
-                                <div class="img">
-                                    <img src="/public/images/step-img1.png" alt="">
-                                </div>
-                            </div>
-                            <div class="navct-desc" id="order2" style="display: none;">
-                                <div class="icon">
-                                    <img src="/public/images/services-icon1.png" alt="">
-                                </div>
-                                <h3 class="hd">Cài đặt công cụ mua hàng</h3>
-                                <p>
-                                    Click vào cài đặt công cụ đặt hàng của Nhất Nhất Logistics. Công cụ hỗ trợ đặt hàng các website taobao, tmall, 1688.
-                                </p>
-                                <div class="img">
-                                    <img src="/public/images/step-img1.png" alt="">
-                                </div>
-                            </div>
-                            <div class="navct-desc" id="order3" style="display: none;">
-                                <h3 class="hd">CHỌN HÀNG VÀ THÊM VÀO GIỎ</h3>
-                                <p>
-                                    Truy cập vào các trang mua sắm Taobao.com, Tmall.com, 1688.com … chọn hàng và thêm hàng vào giỏ.
-                                </p>
-                                <div class="img">
-                                    <img src="/public/images/step-img1.png" alt="">
-                                </div>
-                            </div>
-                            <div class="navct-desc" id="order4" style="display: none;">
-                                <h3 class="hd">GỬI ĐƠN HÀNG</h3>
-                                <p>
-                                    Quay lại website nhatnhatlogistics.com và kiểm tra giỏ hàng Click vào “Gửi đơn hàng” để tạo đơn hàng,chờ xác nhận đặt hàng thành công.
-                                </p>
-                                <div class="img">
-                                    <img src="/public/images/step-img1.png" alt="">
-                                </div>
-                            </div>
-                            <div class="navct-desc" id="order5" style="display: none;">
-                                <h3 class="hd">ĐẶT CỌC TIỀN HÀNG</h3>
-                                <p>
-                                    Kiểm tra đơn hàng và đặt cọc tiền hàng qua hình thức chuyển khoản hoặc trực tiếp tại các văn phòng giao dịch gần nhất của Nhất Nhất Logistics.
-                                </p>
-                                <div class="img">
-                                    <img src="/public/images/step-img1.png" alt="">
-                                </div>
-                            </div>
-                            <div class="navct-desc" id="order6" style="display: none;">
-                                <h3 class="hd">GỬI HÀNG VÀ THANH TOÁN</h3>
-                                <p>
-                                    Quý khách nhận được thông báo hàng về Việt Nam. Quý khách thanh toán số tiền còn  thiếu qua hình thức chuyển khoản hoặc trực tiếp. Sau khi thanh toán quý khách hàng có thể yêu cầu ship hàng hoặc trực tiếp nhận hàng tại kho của Nhất Nhất Logistics.
-                                </p>
-                                <div class="img">
-                                    <img src="/public/images/step-img1.png" alt="">
-                                </div>
-                            </div>
-                        </div>
+                </div>
+                <div class="service-item">
+                    <div class="service-item-heaader">
+                        <img src="https://vanchuyenphongvu.vn/wp-content/uploads/2019/08/icon-service-3.png" alt=""/>
+                        <p class="capture">DỊCH VỤ ĐỔI TIỀN</p>
+                    </div>
+                    <div class="service-item-content">
+                        <p class="sub-capture">Hỗ trợ quý khách hàng nạp tiền vào tài khoản Alipay nhanh chóng, an toàn. Đổi tiền Trung – Việt tỷ giá thấp.</p>
+                        <ul>
+                            <li>Nạp tiền nhanh chóng, tiện lợi</li>
+                            <li>Phí dịch vụ thấp nhất thị trường</li>
+                            <li>Tỷ giá thấp</li>
+                        </ul>
                     </div>
                 </div>
-            </div>
-        </div>
-        <div class="benefits">
-            <div class="all">
-                <div class="sec-title">
-                    <h2 class="hd">Quyền lợi của
-                        <br>
-                        khách hàng</h2>
-                    <p>Businesses often become known today through effective marketing. The marketing...</p>
+                <div class="service-item">
+                    <div class="service-item-heaader">
+                        <img src="https://vanchuyenphongvu.vn/wp-content/uploads/2019/08/icon-service-4.png" alt=""/>
+                        <p class="capture">DỊCH VỤ DẪN KHÁCH ĐI ĐÁNH HÀNG</p>
+                    </div>
+                    <div class="service-item-content">
+                        <p class="sub-capture">Phiên dịch thông thạo các địa chỉ nhập hàng uy tín. Ghép nhóm đánh hàng tiết kiệm chi phí cho các chủ thể kinh doanh</p>
+                        <ul>
+                            <li>70% nhân sự là người Việt ở Trung Quốc lâu năm</li>
+                            <li>Quan hệ tốt với các xưởng nhập hàng</li>
+                            <li>Thông thạo địa hình các chợ nhập hàng</li>
+                        </ul>
+                    </div>
                 </div>
-                <ul class="list-benefits">
-                    <li class="benefit__item">
-                        <div class="benefit-card">
-                            <div class="img">
-                                <a href="javascript:;">
-                                    <img src="/public/images/benefit-img1.jpg" alt="">
-                                </a>
-                            </div>
-                            <div class="content">
-                                <h4 class="hd"><a href="">Khách hàng thân thiết</a></h4>
-                                <p>
-                                    Tích điểm thành viên từ mỗi đơn hàng và nhận được ưu đãi về phí và nhiều ưu đãi khác cho các đơn sau
-                                </p>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="benefit__item">
-                        <div class="benefit-card">
-                            <div class="img">
-                                <a href="javascript:;">
-                                    <img src="/public/images/benefit-img2.jpg" alt="">
-                                </a>
-                            </div>
-                            <div class="content">
-                                <h4 class="hd"><a href="">Marketing &amp; Bán hàng</a></h4>
-                                <p>
-                                    Hỗ trợ Quý khách kết nối tới các giải pháp Marketing &amp; bán hàng, cung cấp hệ sinh thái đầy đủ từ nhập hàng tới bán hàng và phân phối.
-                                </p>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="benefit__item">
-                        <div class="benefit-card">
-                            <div class="img">
-                                <a href="javascript:;">
-                                    <img src="/public/images/benefit-img3.jpg" alt="">
-                                </a>
-                            </div>
-                            <div class="content">
-                                <h4 class="hd"><a href="">Ưu đãi theo sản lượng tháng</a></h4>
-                                <p>
-                                    Hoàn phí vận chuyển theo sản lượng đặt hàng theo từng tháng
-                                </p>
-                            </div>
-                        </div>
-                    </li>
-                </ul>
             </div>
         </div>
     </main>
